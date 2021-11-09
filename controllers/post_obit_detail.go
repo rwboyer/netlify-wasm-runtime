a9abb108c2d7f639012838649f5e689f396bb2de
Robert W Boyer <rwboyer@mac.com>
2021-11-09 14:35:23 -0500

    post vigil working without image

--- controllers/post_obit_detail.go
+++ controllers/post_obit_detail.go
@@ -2,50 +2,56 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
+	_ "fmt"
 	"log"
-	_ "log"
 	"net/http"
-
+	_ "mime/multipart"
 	_ "github.com/go-chi/chi/v5"
 	"github.com/rwboyer/ginapi/models"
 )
 
 func PostObitDetail() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var vigils []models.Vigil
 		var vigil models.Vigil
 
 		//obit := chi.URLParam(r, "ref")
 		o := r.URL.Query().Get("ref")
+		//json.NewDecoder(r.Body).Decode(&vigil)
+		r.ParseMultipartForm(32 << 20)
+		vigil.Name = r.MultipartForm.Value["name"][0]
+		vigil.Email = r.MultipartForm.Value["email"][0]
+		vigil.Phone = r.MultipartForm.Value["phone"][0]
+		if r.MultipartForm.Value["candle"] != nil {
+			vigil.Candle = "ON"
+		} else{
+			vigil.Candle = "OFF"
+		}
+		vigil.Text = r.MultipartForm.Value["message"][0]
+		vigil.Obit = o
 
+		
 		log.Println(o)
 	
-		rows, err := models.Db.Query("select * from vigil_log where obit = ?;", o)
+		sqlStatement := `
+		INSERT INTO vigil_log (obit, name, email, phone, text, candle, img)
+		VALUES (?, ?, ?, ?, ?, ?, ?)`
+		_, err := models.Db.Exec(sqlStatement, 
+			vigil.Obit, 
+			vigil.Name, 
+			vigil.Email,
+			vigil.Phone,
+			vigil.Text,
+			vigil.Candle,
+			vigil.Img,
+		)
 		if err != nil {
-			fmt.Println(err.Error())
+			log.Println(err)
 		}
-		for rows.Next() {
-			err := rows.Scan(
-				&vigil.Id,
-				&vigil.Date,
-				&vigil.Obit,
-				&vigil.Name,
-				&vigil.Email,
-				&vigil.Phone,
-				&vigil.Text,
-				&vigil.Candle,
-				&vigil.Img)
-			vigils = append(vigils, vigil)
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-		}
-		defer rows.Close()
+
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
 
-		j, _ := json.Marshal(vigils)
+		j, _ := json.Marshal(vigil)
 		w.Write(j)
 	}
 }
