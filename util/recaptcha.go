2410e8fda11b8fd7929a239d0b8a52b227897425
Robert W Boyer <rwboyer@mac.com>
2021-11-13 13:23:39 -0500

    recaptcha util function

---
+++ util/recaptcha.go
@@ -0,0 +1,49 @@
+package util
+
+import(
+	"net/http"
+	"encoding/json"
+	"time"
+	"errors"
+)
+
+type SiteVerifyResponse struct {
+	Success     bool      `json:"success"`
+	Score       float64   `json:"score"`
+	Action      string    `json:"action"`
+	ChallengeTS time.Time `json:"challenge_ts"`
+	Hostname    string    `json:"hostname"`
+	ErrorCodes  []string  `json:"error-codes"`
+}
+
+const siteVerifyURL = "https://www.google.com/recaptcha/api/siteverify"
+
+func CheckRecaptcha(secret, response string) error {
+
+	req, err := http.NewRequest(http.MethodPost, siteVerifyURL, nil)
+	if err != nil {
+		return err
+	}
+
+	q := req.URL.Query()
+	q.Add("secret", secret)
+	q.Add("response", response)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body := SiteVerifyResponse{}
+	if err = json.NewDecoder(resp.Body).Decode(&body); err != nil {
+		return err
+	}
+
+	// Check recaptcha verification success.
+	if !body.Success {
+		return errors.New("unsuccessful recaptcha verify request")
+	}
+	return nil
+}
